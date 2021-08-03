e3a0cb644638a4252b9f205ff79d340a96167ced
Raasaal Yuosop <raasaal511@gmail.com>
2021-08-03 09:22:09 +0300

    add django-gebug-toolbar, add menu_tags.py ...

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -8,6 +9,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor', include('ckeditor_uploader.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
     path('', include('blog.urls')),
 ]
 
